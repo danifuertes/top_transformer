1822d1ca496945a19fea82897bc118ac7248d6ca
Daniel Fuertes <d.fcoiras@upm.es>
2025-01-23 17:08:16 +0100

    Streamlit demo functional

--- app.py
+++ app.py
@@ -10,10 +10,11 @@ from utils.functions import load_problem
 from visualize import reshape_tours, add_depots, assign_colors
 from nets.attention_model import AttentionModel
 from nets.pointer_network import PointerNetwork
+from nets.gamma import GAMMA
 from nets.gpn import GPN
 
 
-def load_scenario(seed, problem, graph_size, data_distribution, num_agents, num_depots, max_length, device):
+def load_scenario(seed, problem, graph_size, data_distribution, num_agents, num_depots, max_length):
     
     # Set seed for reproducibility
     set_seed(seed)
@@ -50,6 +51,8 @@ def apply_model(model, num_agents, num_depots, graph_size, inputs, dataset, retu
         model.to(device)
         if isinstance(model, AttentionModel):
             model_name = 'Transformer'
+        elif isinstance(model, GAMMA):
+            model_name = 'GAMMA'
         elif isinstance(model, PointerNetwork):
             model_name = 'Pointer'
         else:
@@ -64,15 +67,11 @@ def apply_model(model, num_agents, num_depots, graph_size, inputs, dataset, retu
         _, _, tours = model(inputs, return_pi=True)
 
         # Reshape tours list
-        print(tours)
         tours = tours.cpu().detach().numpy().squeeze()
-        print(tours)
         tours = reshape_tours(tours, num_agents, end_ids=graph_size + 1)
-        print(inputs['loc'].shape[0] + 1)
 
     # Add depots and print tours
     tours = add_depots(tours, num_agents, graph_size)
-    
     return tours, inputs, model_name
     
 
@@ -85,7 +84,7 @@ def plot(tours, scenario, problem, model_name, data_dist):
         scenario[k] = scenario[k].squeeze()
     
     # Plot
-    fig, ax = plot_tour_streamlit(tours, scenario, problem, model_name, data_dist)
+    plot_tour_streamlit(tours, scenario, problem, model_name, data_dist)
 
 
 def plot_tour_streamlit(tours, inputs, problem, model_name, data_dist=''):
@@ -93,7 +92,7 @@ def plot_tour_streamlit(tours, inputs, problem, model_name, data_dist=''):
     Plot a given tour using Plotly for Streamlit.
     """
     # Number of agents
-    num_agents = len(tours)
+    num_agents = len(tours) if len(tours) > 0 else 1
     colors = assign_colors(num_agents) if num_agents <= 6 else np.random.rand(num_agents, 3)
 
     # Depot and locations
@@ -101,6 +100,7 @@ def plot_tour_streamlit(tours, inputs, problem, model_name, data_dist=''):
     depot2 = inputs['depot2'] if 'depot2' in inputs else depot
     loc = inputs['loc']
     prizes = inputs['prize']
+    max_length = inputs['max_length']
 
     # Create a Plotly figure
     fig = go.Figure()
@@ -122,8 +122,10 @@ def plot_tour_streamlit(tours, inputs, problem, model_name, data_dist=''):
         prizes = np.concatenate(([0], inputs['prize'], [0]), axis=0)
 
     # Add tours
+    num_nodes, reward, length = 0, 0, 0
     for k, tour in enumerate(tours):
         nodes = np.take(loc, tour, axis=0)
+        num_nodes += len(nodes) - 2
         x_coords = nodes[:, 0]
         y_coords = nodes[:, 1]
 
@@ -133,12 +135,14 @@ def plot_tour_streamlit(tours, inputs, problem, model_name, data_dist=''):
             line=dict(color=f'rgb({colors[k][0] * 255}, {colors[k][1] * 255}, {colors[k][2] * 255})', width=2),
             name=f'Agent {k + 1}'
         ))
+        
+        reward += prizes[tour].sum()
+        length += np.sum(np.linalg.norm(nodes[1:] - nodes[:-1], axis=1))
 
     # Set title
-    title = f"{problem.upper()} {num_agents}"
-    if data_dist:
-        title += f" ({data_dist.lower()})"
-    title += f" - {model_name}: Max length = ..."
+    title = f"Number of Nodes: {num_nodes} / {len(loc) - 2}        \
+              Path length: {length:.2f} / {num_agents * max_length:.0f}        \
+              Reward collected: {reward:.2f} / {prizes.sum():.0f}"
     fig.update_layout(title=title, xaxis_title="X-axis", yaxis_title="Y-axis", showlegend=True)
 
     # Render the plot in Streamlit
@@ -149,44 +153,59 @@ def plot_tour_streamlit(tours, inputs, problem, model_name, data_dist=''):
 def main():
     st.title("Team Orienteering Problem")
 
+    # Initialize session state for tours
+    st.session_state.tours = []
+    model_name = "Scenario"
+
     # Sidebar for input parameters
     st.sidebar.header("Input Parameters")
     seed = st.sidebar.number_input("Random Seed", value=0)
-    model = st.sidebar.text_input("Model Path or Baseline", value="opga")
+    model = st.sidebar.selectbox("Model", ["Transformer", "Pointer Network", "Graph Pointer Network", "GAMMA", "Genetic Algorithm", "Compass", "ACO", "PSO", "Gurobi"], index=0)
     timeout = 0  # st.sidebar.number_input("Timeout (seconds)", value=0)
     problem = "top"
-    graph_size = st.sidebar.number_input("Graph Size", value=20)
-    data_distribution = st.sidebar.text_input("Data Distribution", value="const")
-    num_agents = st.sidebar.number_input("Number of Agents", value=2)
+    graph_size = st.sidebar.number_input("Graph Size", value=20, min_value=20, max_value=100)
+    data_distribution = st.sidebar.selectbox("Reward Distribution", ["Constant", "Uniform", "Distance to depot"], index=0)
+    # num_agents = st.sidebar.number_input("Number of Agents", value=2, min_value=2, max_value=5)
     num_depots = 1
     return2depot = True
     max_length = 2.0  # st.sidebar.number_input("Max Length", value=2.0)
     use_cuda = st.sidebar.checkbox("Use CUDA", value=True)
-    device = torch.device("cuda:0" if use_cuda else "cpu")
+    
+    # Map inputs to real values
+    data_distribution = {"Constant": "const", "Uniform": "unif", "Distance to depot": "dist"}[data_distribution]
+    model = {"Transformer": "transformer", "Pointer Network": "pointer", "Graph Pointer Network": "gpn", "GAMMA": "gamma", "Genetic Algorithm": "opga", "Compass": "compass", "ACO": "aco", "PSO": "pso", "Gurobi": "gurobi"}[model]
+    model_graph_size = 20 if graph_size <= 30 else (50 if graph_size <= 75 else 100)
+    num_agents = 2 if model_graph_size == 20 else (3 if model_graph_size == 50 else 5)
+    if model in ('transformer', 'pointer', 'gpn', 'gamma'):
+        if model == 'transformer':
+            model_model = 'attention'
+        elif model == 'pn':
+            model_model = 'pointer'
+        else:
+            model_model = model
+        model = f"pretrained/{problem}_{data_distribution}{model_graph_size}/{model_model}_rollout_{num_agents}agents_L{max_length}"
 
     # Load the scenario and dataset
-    scenario, dataset = load_scenario(seed, problem, graph_size, data_distribution, num_agents, num_depots, max_length, device)
-
-    # Always plot the scenario
-    plot([], scenario, problem, "Scenario", data_distribution)
+    scenario, dataset = load_scenario(seed, problem, graph_size, data_distribution, num_agents, num_depots, max_length)
 
     # Buttons for calculating and cleaning
-    col1, col2 = st.columns(2)
+    col1, col2 = st.sidebar.columns(2)
     with col1:
-        calculate = st.button("Calculate")
+        calculate = st.sidebar.button("Calculate", type='primary')
     with col2:
-        clean = st.button("Clean")
+        clean = st.sidebar.button("Clean")
 
     # Calculate button action
     if calculate:
         tours, scenario, model_name = apply_model(model, num_agents, num_depots, graph_size, scenario, dataset, return2depot, timeout, use_cuda)
-        
-        # Plot the scenario with routes
-        plot(tours, scenario, problem, model_name, data_distribution)
+        st.session_state.tours = tours
 
     # Clean button action
     if clean:
-        pass
+        st.session_state.tours = []
+        
+    # Plot the scenario with routes
+    plot(st.session_state.tours, scenario, problem, model_name, data_distribution)
 
 if __name__ == "__main__":
     main()
